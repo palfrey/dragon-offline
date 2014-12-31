04fa2b29a1b5c30de77ccf1912e0c1f2826bf26f
Tom Parker <palfrey@tevp.net>
2014-12-31 12:20:32 +0000

    Basic game display

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@ angular.module('dragon-offline', ['ionic'])
 .config(['$httpProvider', function($httpProvider) {
   $httpProvider.defaults.withCredentials = true;
 }])
-.run(function($ionicPlatform, $http, $log) {
+.run(function($ionicPlatform, $http, $log, $rootScope) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -20,24 +20,49 @@ angular.module('dragon-offline', ['ionic'])
 
     $dragon = "http://www.dragongoserver.net/";
 
-    $db = new PouchDB("games");
-    $login = $db.get("last-games", function(err, doc) {
-      if (doc == null) {
-          $http.get($dragon + "login.php?quick_mode=1&userid=" + $username + "&passwd=" + $password).
+    function getDoc(onDone, doc) {
+      $http.get($dragon + "login.php?quick_mode=1&userid=" + $username + "&passwd=" + $password).
+        success(function(data, status, headers, config) {
+          $log.info(status);
+          $http.get($dragon + "quick_do.php?obj=game&cmd=list&view=running").
             success(function(data, status, headers, config) {
-              $log.info(status);
-              $http.get($dragon + "quick_do.php?obj=game&cmd=list&view=running").
-                success(function(data, status, headers, config) {
-                  $log.info(status);
-                  $log.info(data);
-                  data["when"] = new Date().toISOString();
-                  $db.put(data, "last-games", function (err, response) { });
-                });
-            }).
-            error(function(data, status, headers, config) {
+              data["when"] = new Date().toISOString();
+              $rev = doc == null? null: doc._rev;
+              $db.put(data, "running-games", $rev, function (err, response) { onDone(data); });
             });
-      }
+        }).
+        error(function(data, status, headers, config) {
+        });
+    }
+
+    function useDoc(doc) {
       $log.info(doc);
+      headers = doc.list_header;
+      $rootScope.games = [];
+      for (var key in doc.list_result) {
+        item = doc.list_result[key];
+        hash = {};
+        for (i=0;i<headers.length;i++) {
+          hash[headers[i]] = item[i];
+        }
+        $log.info(hash);
+        $rootScope.games.push({"white_player" : hash["white_user.id"], "black_player" : hash["black_user.id"], "next_player": hash["move_uid"]})
+      }
+      $log.info($rootScope.games)
+      $rootScope.$digest();
+    }
+
+    $db = new PouchDB("games");
+    $login = $db.get("running-games", function(err, doc) {
+      if (doc == null) {
+        getDoc(useDoc);
+      }
+      else if ((new Date() - Date.parse(doc["when"])) > 1000*60*20) { // 20 minutes
+        getDoc(useDoc, doc);
+      }
+      else {
+         useDoc(doc);
+      }
     });
   });
 })
